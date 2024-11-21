b6c3f5fced5353c2a6e019953f377ddf0b969f38
DeltDev <deltenc88@gmail.com>
2024-11-21 19:19:03 +0700

    feat: update schedule

--- models/Schedule.py
+++ models/Schedule.py
@@ -5,12 +5,34 @@ from models.Transaction import Transaction
 class Schedule:
     transactionList: list[Transaction] #daftar transaksi
     resourceList: list[Resource] #daftar resource
-    operationList: list[Operation] #daftar operasi
+    operationQueue: list[Operation] #daftar operasi
 
     def __init__(self, operationList: list, dataItemList: list, transactionList: list):
         self.operationList = operationList
         self.dataItemList = dataItemList
         self.transactionList = transactionList
+    #Method Getter
+    def getTransactionList(self):
+        return self.transactionList
+    def getResourceList(self):
+        return self.resourceList
+    def getOperationQueue(self):
+        return self.operationQueue
+    
+    #Method setter
+    def setTransactionList(self,txList:list[Transaction]):
+        self.transactionList = txList
+    def setResourceList(self,resList:list[Resource]):
+        self.resourceList = resList
+    def setOperationQueue(self,opQueue:list[Operation]):
+        self.operationQueue = opQueue 
 
-    def dequeue(self) -> Operation:
+    #method tambahan
+    def addTransaction(self,tx:Transaction):
+        self.transactionList.append(tx)
+    def addResource(self,res:Resource):
+        self.resourceList.append(res)
+    def enqueueOperation(self,op:Operation):
+        self.operationQueue.append(op)
+    def dequeueOperation(self) -> Operation:
         return self.operationList.pop(0)
