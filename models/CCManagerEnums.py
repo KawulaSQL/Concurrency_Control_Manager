2afd9a7be5fdb45d009f930b48f5337bc67ad6e8
DeltDev <deltenc88@gmail.com>
2024-11-21 17:34:28 +0700

    feat: update Operation class

--- models/CCManagerEnums.py
+++ models/CCManagerEnums.py
@@ -4,3 +4,14 @@ class LockType(Enum):
     S = "Shared"
     X = "Exclusive"
     N = "None"
+class OperationType(Enum):
+    R = "Read"
+    W = "Write"
+    SL = "Shared Lock"
+    XL = "Exclusive Lock"
+    UL = "Unlock"
+    C = "Commit"
+    A = "Abort"
+class OperationStatus(Enum):
+    E = "Executed"
+    NE = "Not Executed"
