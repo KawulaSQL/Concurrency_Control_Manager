94aad0490370c28feb977c91e0ce5870cf1c9eee
Dabbir <ahmadmudabbir03@gmail.com>
2024-12-11 18:18:47 +0700

    feat (lock-based): implement log_object, validate_object, and end_transaction

--- models/TwoPhaseLocking.py
+++ models/TwoPhaseLocking.py
@@ -1,17 +1,18 @@
 from collections import defaultdict
 from abc import ABC
+from models.Schedule import Schedule
+from models.Resource import Resource
+from models.Response import Response
+from models.Operation import Operation
+from models.ControllerMethod import ControllerMethod
+from models.Transaction import Transaction
+from models.CCManagerEnums import OperationType, ResponseType, TransactionStatus
 
-from Response import Response
-from CCManagerEnums import Action
-
-from ControllerMethod import ControllerMethod
-from Transaction import Transaction,Operation,Resource
 class TwoPhaseLocking(ControllerMethod, ABC):
     def __init__(self):
         self.schedule = Schedule()
 
-    def log_object(self, Operation: operation): 
-
+    def log_object(self, operation: Operation): 
         """
         locking object/resource
         """
@@ -21,6 +22,19 @@ class TwoPhaseLocking(ControllerMethod, ABC):
         #     "operation": "LOG",
         #     "status": "Logged"
         # })
+
+        print(f"Logging operation for resource: {operation.getOperationResource()}")
+
+        transaction = self.schedule.getTransactionByID(operation.getOpTransactionID())
+        operationResource = self.schedule.get_or_create_resource(operation.getOperationResource())
+
+        # Give Lock to the object/resource
+        if operation.getOperationType() == OperationType.R:
+            print(f"Setting resource read lock for {operationResource.getName()} to the transaction-{transaction.getTransactionID()}.")
+            operationResource.addLockHolder(transaction.getTransactionID(), "S", "Locked")
+        elif operation.getOperationType() == OperationType.W:
+            print(f"Setting resource write lock for {operationResource.getName()} to the transaction-{transaction.getTransactionID()}.")
+            operationResource.addLockHolder(transaction.getTransactionID(), "X", "Locked")
 
     def validate_object(self, operation: Operation) -> Response: #rough implementation
         """
@@ -34,10 +48,44 @@ class TwoPhaseLocking(ControllerMethod, ABC):
         #     success = False
         # return Response(success=success, message="Validation successful" if success else "Validation failed")
 
+        print(f"Validating operation to get resource: {operation.getOperationResource()}")
+
+        # Creating/validating the Resource object of the requested resource name
+        operationResource = self.schedule.get_or_create_resource(operation.getOperationResource())
+        print(f"Operation resource: {operationResource.getName()}")
+
+        transaction = self.schedule.getTransactionByID(operation.getOpTransactionID())
+        print(f"Transaction retrieved: {transaction.getTransactionID()}, Timestamp: {transaction.getTimestamp()}")
+
+        transaction.addOperation(operation)
+
+        # Use the lock type to determine if the transaction can proceed
+        if operation.getOperationType() == OperationType.R:
+            if not self.shared_lock(transaction.getTransactionID(), operationResource.getName()):
+                print("Transaction cannot acquire shared lock. Transaction aborted.")
+                transaction.setTransactionStatus(TransactionStatus.ABORTED)
+                return Response(ResponseType.ABORT, operation)
+        elif operation.getOperationType() == OperationType.W:
+            if not self.exclusive_lock(transaction.getTransactionID(), operationResource.getName()):
+                print("Transaction cannot acquire exclusive lock. Transaction aborted.")
+                transaction.setTransactionStatus(TransactionStatus.ABORTED)
+                return Response(ResponseType.ABORT, operation)
+        return Response(ResponseType.ALLOWED, operation)
+
     def end_transaction(self, transaction_id: int):
         """
         Implementation
         """
+        print(f"Ending transaction {transaction_id}.")
+
+        transaction = self.schedule.getTransactionByID(transaction_id)
+        
+        # Release all locks held by the transaction but check if the transaction is still active first or if it has been aborted
+        if transaction.getTransactionStatus() == TransactionStatus.ACTIVE:
+            self.release_locks(transaction)
+            transaction.setTransactionStatus(TransactionStatus.COMMITTED)
+        else:
+            self.abort(transaction_id)
 
         
     def shared_lock(self, transaction: int, table: str) -> bool:
@@ -103,6 +151,10 @@ class TwoPhaseLocking(ControllerMethod, ABC):
         #     self.log_transaction(transaction, table, current["operation"], "Queue")
         # else:
         #     self.abort(current)
+
+    def wound_wait(self, current: dict):
+        """Deadlock prevention using the wound-wait scheme."""
+        
 
     # def commit(self, transaction_id: int):
     #     """Commit a transaction and release its locks."""
